6cb6a71074cb84b1884473a207e9b710d8ce2ed6
MetalSnow <aymanetrend@gmail.com>
2025-08-03 00:06:29 +0800

    feat: Implement post routes and controllers

---
+++ backend/controllers/postController.js
@@ -0,0 +1,60 @@
+const { PrismaClient } = require('../generated/prisma');
+const asyncHandler = require('express-async-handler');
+
+const prisma = new PrismaClient();
+
+const getAllPosts = asyncHandler(async (req, res) => {
+  const posts = await prisma.post.findMany();
+  res.json({ data: posts });
+});
+
+const getPost = asyncHandler(async (req, res) => {
+  const postId = req.params.postId;
+  const post = await prisma.post.findUnique({
+    where: {
+      id: postId,
+    },
+  });
+  res.json({ data: post });
+});
+
+const createPost = asyncHandler(async (req, res) => {
+  const createdPost = await prisma.post.create({
+    data: {
+      title: req.body.title,
+      content: req.body.content,
+    },
+  });
+
+  res.json({ message: 'Post created', data: createdPost });
+});
+
+const updatePost = asyncHandler(async (req, res) => {
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: req.params.postId,
+    },
+    data: {
+      title: req.body.title,
+      content: req.body.content,
+    },
+  });
+  res.json({ message: 'Post Updated', data: updatedPost });
+});
+
+const deletePost = asyncHandler(async (req, res) => {
+  const deletedPost = await prisma.post.delete({
+    where: {
+      id: req.params.postId,
+    },
+  });
+  res.json({ message: 'Post deleted', data: deletedPost });
+});
+
+module.exports = {
+  getAllPosts,
+  getPost,
+  updatePost,
+  deletePost,
+  createPost,
+};
