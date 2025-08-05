5a42ec9e3334d3297ae81d6dab3dec358fb3fc2c
MetalSnow <aymanetrend@gmail.com>
2025-08-05 21:57:26 +0800

    feat: Add user sign-up route and bcrypt for password hashing

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -2,11 +2,14 @@ const { Router } = require('express');
 const {
   getUserInfo,
   updateUserInfo,
+  signupUser,
 } = require('../controllers/userController');
 const userRouter = Router();
 
 userRouter.get('/', getUserInfo);
-
 userRouter.patch('/update', updateUserInfo);
+
+//authentication
+userRouter.post('/sign-up', signupUser);
 
 module.exports = userRouter;
