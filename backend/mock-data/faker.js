b844c3e169a4a14eb72db3f23675c30cf10a1a22
MetalSnow <aymanetrend@gmail.com>
2025-08-17 23:53:43 +0800

    feat: Create fake posts data using faker package

---
+++ backend/mock-data/faker.js
@@ -0,0 +1,16 @@
+const { faker } = require('@faker-js/faker');
+
+function createRandomPosts() {
+  return {
+    title: faker.book.title(),
+    content: faker.lorem.paragraphs(10),
+    published: true,
+    authorId: 3,
+  };
+}
+
+const posts = faker.helpers.multiple(createRandomPosts, {
+  count: 7,
+});
+
+module.exports = posts;
