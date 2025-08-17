8f60e21281728057d50f7421d73468d4fe2415eb
MetalSnow <aymanetrend@gmail.com>
2025-08-17 18:27:09 +0800

    feat: Render each article info based on params

--- frontend-one/src/components/article/Article.jsx
+++ frontend-one/src/components/article/Article.jsx
@@ -1,20 +1,34 @@
 import useFetch from '../../hooks/useFetch';
+import { Link, useParams } from 'react-router-dom';
+import Content from './Content';
 
 const Article = () => {
   const { data, loading, error } = useFetch('http://localhost:3000/posts');
 
+  const { id } = useParams();
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
-
   return (
-    <>
-      <h2>Articles</h2>
-      <ul>
-        {data.map((el) => {
-          return <li key={el.id}>{el.title}</li>;
-        })}
-      </ul>
-    </>
+    <div>
+      {!id ? (
+        <>
+          <h2>Articles</h2>
+          <ul>
+            {data.map((el) => {
+              return (
+                <li key={el.id}>
+                  Topic:
+                  <Link to={`/articles/${el.id}`}> {el.title}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      ) : (
+        <Content data={data} id={id} />
+      )}
+    </div>
   );
 };
 
