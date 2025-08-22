5f9a8c68cf95238a6a29f8f03afd3f96885ad0a6
MetalSnow <aymanetrend@gmail.com>
2025-08-22 23:06:46 +0800

    style: Finish styling components

--- frontend-one/src/components/article/Article.jsx
+++ frontend-one/src/components/article/Article.jsx
@@ -1,36 +1,40 @@
 import useFetch from '../../hooks/useFetch';
 import { Link, useParams } from 'react-router-dom';
 import Content from './Content';
-import { ArrowBigLeft } from 'lucide-react';
+import styles from './Article.module.css';
 import Header from '../header/Header';
+import { LoaderCircle } from 'lucide-react';
 
 const Article = () => {
   const { data, loading, error } = useFetch('http://localhost:3000/posts');
 
   const { id } = useParams();
 
-  if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
   return (
     <>
       <Header />
-      {!id ? (
-        <div>
-          <h2>Articles</h2>
-          <ul>
-            {data.map((el) => {
-              return (
-                <li key={el.id}>
-                  Topic:
-                  <Link to={`/articles/${el.id}`}> {el.title}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      ) : (
-        <Content data={data} id={id} />
-      )}
+      <div className={styles.container}>
+        {loading ? (
+          <LoaderCircle size={30} strokeWidth={2.5} className={styles.loader} />
+        ) : !id ? (
+          <>
+            <h2>Articles</h2>
+            <ul>
+              {data.map((el) => {
+                return (
+                  <li key={el.id}>
+                    Topic:
+                    <Link to={`/articles/${el.id}`}> {el.title}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </>
+        ) : (
+          <Content data={data} id={id} />
+        )}
+      </div>
     </>
   );
 };
