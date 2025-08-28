e6a7f1947c7c1448129aff5ecbf4e7c50fc1f911
MetalSnow <aymanetrend@gmail.com>
2025-08-28 12:45:56 +0800

    feat: Implement user login and validation
    - Retrieve authentication token from the backend on successful login
    - Validate login form based on server response

---
+++ frontend-two/src/hooks/usePost.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+const usePost = (url) => {
+  const [loading, setLoding] = useState(false);
+  const [error, setError] = useState(null);
+  const [validation, setValidation] = useState(null);
+
+  const authenticate = async (data) => {
+    setError(null);
+    setValidation(null);
+    setLoding(true);
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      console.log(response);
+
+      if (response.status === 500) {
+        throw new Error(`HTTP error! status code:${response.status} `);
+      }
+
+      if (response.status === 401) {
+        const result = await response.json();
+        setValidation(result.error);
+        setLoding(false);
+        return;
+      }
+
+      const result = await response.json();
+      setLoding(false);
+      return result.token;
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  return { authenticate, loading, error, validation };
+};
+
+export default usePost;
