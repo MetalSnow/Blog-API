216c3e85a6a7bd7222ab4a8acaf027b508cbb215
MetalSnow <aymanetrend@gmail.com>
2025-10-05 22:34:15 +0800

    fix: Configure CORS with Specific Origins

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,16 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'https://blog-api-ruddy-mu.vercel.app',
+      'https://blog-api-yygg.vercel.app',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
